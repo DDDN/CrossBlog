dc29cf7ba6668dae4cf531a2df64cdf032f3a3bd
Lukasz Jaskiewicz <lukasz@jaskiewicz.de>
2017-07-18 12:34:42 +0200

    made ConfigSectionNames class static

--- Blog/Configuration/ConfigSectionNames.cs
+++ Blog/Configuration/ConfigSectionNames.cs
@@ -1,6 +1,6 @@
 ﻿namespace DDDN.CrossBlog.Blog.Configuration
 {
-    public class ConfigSectionNames
+    public static class ConfigSectionNames
     {
         public const string Localization = nameof(Localization);
         public const string Routing = nameof(Routing);

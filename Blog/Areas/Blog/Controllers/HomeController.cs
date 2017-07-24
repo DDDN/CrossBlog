527de4e176643d9b61a8d4dc53d92af852eb2e7d
Lukasz Jaskiewicz <lukasz@jaskiewicz.de>
2017-07-24 15:48:31 +0200

    custom string localizer infrastructure added

--- Blog/Areas/Blog/Controllers/HomeController.cs
+++ Blog/Areas/Blog/Controllers/HomeController.cs
@@ -2,51 +2,50 @@ using DDDN.CrossBlog.Blog.Routing;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.Localization;
 using System.Globalization;
 
 namespace DDDN.CrossBlog.Blog.Areas.Blog.Controllers
 {
-	[Area("Blog")]
-	[MiddlewareFilter(typeof(BlogCulturesMiddlewareFilter))]
-	public class HomeController : Controller
-	{
-		public IHttpContextAccessor _httpContextAccessor;
-		private IActionDescriptorCollectionProvider _descriptorCollectionProvider;
+    [Area("Blog")]
+    [MiddlewareFilter(typeof(BlogCulturesMiddlewareFilter))]
+    public class HomeController : Controller
+    {
+        public IStringLocalizer _homeLocalizer;
 
-		public HomeController(
-			 IHttpContextAccessor httpContextAccessor,
-			 IActionDescriptorCollectionProvider descriptorCollectionProvider)
-		{
-			_httpContextAccessor = httpContextAccessor;
-			_descriptorCollectionProvider = descriptorCollectionProvider;
-		}
+        public HomeController(IStringLocalizer<HomeController> homeLocalizer)
+        {
+            _homeLocalizer = homeLocalizer ?? throw new System.ArgumentNullException(nameof(homeLocalizer));
+        }
 
-		public IActionResult Index()
-		{
-			ViewBag.Me = HttpContext.Request.Path;
+        public IActionResult Index()
+        {
+            ViewBag.Me = HttpContext.Request.Path;
+            ViewBag.CurrentCultureName = CultureInfo.CurrentCulture.Name;
+            ViewBag.Test1 = _homeLocalizer["Test1"];
 
-			return View();
-		}
+            return View();
+        }
 
-		public IActionResult Newest()
-		{
-			ViewBag.Me = HttpContext.Request.Path;
+        public IActionResult Newest()
+        {
+            ViewBag.Me = HttpContext.Request.Path;
 
-			return View();
-		}
+            return View();
+        }
 
-		public IActionResult Archive()
-		{
-			ViewBag.Me = HttpContext.Request.Path;
+        public IActionResult Archive()
+        {
+            ViewBag.Me = HttpContext.Request.Path;
 
-			return View();
-		}
+            return View();
+        }
 
-		public IActionResult About()
-		{
-			ViewBag.Me = HttpContext.Request.Path;
+        public IActionResult About()
+        {
+            ViewBag.Me = HttpContext.Request.Path;
 
-			return View();
-		}
-	}
+            return View();
+        }
+    }
 }
---
+++ Blog/Localization/OfficeStringLocalizerFactory.cs
@@ -0,0 +1,79 @@
+﻿using DDDN.CrossBlog.Blog.Configuration;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace DDDN.CrossBlog.Blog.Localization
+{
+    public class OfficeStringLocalizerFactory : IStringLocalizerFactory
+    {
+        private readonly IOptions<LocalizationConfigSection> _localizationConfigSection;
+        private readonly ConcurrentDictionary<string, IStringLocalizer> _resourceLocalizations = new ConcurrentDictionary<string, IStringLocalizer>();
+
+
+        public OfficeStringLocalizerFactory(IOptions<LocalizationConfigSection> localizationConfigSection)
+        {
+            _localizationConfigSection = localizationConfigSection ?? throw new ArgumentNullException(nameof(localizationConfigSection));
+        }
+
+        public IStringLocalizer Create(Type resourceSource)
+        {
+            string resourceName = IgnoreOfficeLocalizerFactoryTypeNamespaceLocation(resourceSource.FullName);
+
+            if (_resourceLocalizations.Keys.Contains(resourceName))
+            {
+                return _resourceLocalizations[resourceName];
+            }
+
+            return GetLocalizer(resourceName);
+        }
+
+        private string IgnoreOfficeLocalizerFactoryTypeNamespaceLocation(string resourceSourceFullName)
+        {
+            if (resourceSourceFullName == null)
+            {
+                throw new ArgumentNullException(nameof(resourceSourceFullName));
+            }
+
+            if (_localizationConfigSection.Value.IgnoreOfficeLocalizerFactoryTypeNamespaceLocation
+                & !string.IsNullOrWhiteSpace(_localizationConfigSection.Value.OfficeLocalizerFactoryTypeNamespaceLocation))
+            {
+                return resourceSourceFullName.Replace(
+                    _localizationConfigSection.Value.OfficeLocalizerFactoryTypeNamespaceLocation + ".",
+                    "",
+                    StringComparison.InvariantCultureIgnoreCase);
+            }
+
+            return resourceSourceFullName;
+        }
+
+        public IStringLocalizer Create(string baseName, string location)
+        {
+            string resourceName = IgnoreOfficeLocalizerFactoryTypeNamespaceLocation(baseName);
+
+            if (_resourceLocalizations.Keys.Contains(resourceName))
+            {
+                return _resourceLocalizations[resourceName];
+            }
+
+            return GetLocalizer(resourceName);
+        }
+
+        private IStringLocalizer GetLocalizer(string resourceName)
+        {
+            var officeStringLocalizer = new OfficeStringLocalizer(GetAllFromOfficeFile(resourceName), resourceName);
+            return _resourceLocalizations.GetOrAdd(resourceName, officeStringLocalizer);
+        }
+
+        private Dictionary<string, string> GetAllFromOfficeFile(string resourceKey)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("Test1.pl-PL", "Test1.pl-PLv");
+            dict.Add("Test1.en-US", "Test1.pl-PLv");
+            dict.Add("Test1.de-DE", "Test1.pl-PLv");
+            return dict;
+        }
+    }
+}

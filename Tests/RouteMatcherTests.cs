55538a467688a3d3cde0c073fe4da789bed26074
Lukasz Jaskiewicz <devdone@outlook.com>
2017-07-28 19:46:12 +0200

    first unit test added

---
+++ Tests/RouteMatcherTests.cs
@@ -0,0 +1,33 @@
+﻿using DDDN.CrossBlog.Blog.Routing;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace DDDN.CrossBlog.Blog.Tests.Routing
+{
+	public class RouteMatcherTests
+    {
+        [Fact]
+        public void Match_MatchDefaultRouteTemplate()
+        {
+            // Arrange
+            var router = new RouteMatcher();
+            var defaultTemplate = "{area}/{culture}/{controller}/{action}/{id?}";
+            var path = new Uri("/Blog/en-US/Home/Index", UriKind.Relative).ToString();
+            var routeValues = new RouteValueDictionary()
+            {
+                ["area"] = "Blog",
+                ["culture"] = "en-US",
+                ["controller"] = "Home",
+                ["action"] = "Index"
+            };
+
+            // Act
+            var result = router.Match(defaultTemplate, path);
+
+            // Assert
+            Assert.True(result.All(e => routeValues.Contains(e)));
+        }
+    }
+}

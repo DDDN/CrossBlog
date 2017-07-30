c2ee0190c951d4626dc02cf2a04d5194637bb73f
Lukasz Jaskiewicz <devdone@outlook.com>
2017-07-30 04:11:17 +0200

    OpenXML/DOCX based string resource localization added

--- Blog/Localization/BlogStringLocalizerFactory.cs
+++ Blog/Localization/BlogStringLocalizerFactory.cs
@@ -1,79 +1,99 @@
-﻿using DDDN.CrossBlog.Blog.Configuration;
+﻿/*
+* DDDN.CrossBlog.Blog.Localization.BlogStringLocalizerFactory
+* 
+* Copyright(C) 2017 Lukasz Jaskiewicz
+* Author: Lukasz Jaskiewicz (lukasz@jaskiewicz.de)
+*
+* This program is free software; you can redistribute it and/or modify it under the terms of the
+* GNU General Public License as published by the Free Software Foundation; version 2 of the License.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with this program; if not, write
+* to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+using DDDN.CrossBlog.Blog.Configuration;
+using DDDN.Localization.OpenXML;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 
 namespace DDDN.CrossBlog.Blog.Localization
 {
-    public class BlogStringLocalizerFactory : IStringLocalizerFactory
-    {
-        private readonly IOptions<LocalizationConfigSection> _localizationConfigSection;
-        private readonly ConcurrentDictionary<string, IStringLocalizer> _resourceLocalizations = new ConcurrentDictionary<string, IStringLocalizer>();
+	public class BlogStringLocalizerFactory : IStringLocalizerFactory
+	{
+		private readonly IOptions<LocalizationConfigSection> _localizationConfigSection;
+		private readonly IHostingEnvironment _hostingEnvironment;
+		private readonly ConcurrentDictionary<string, IStringLocalizer> _resourceLocalizations = new ConcurrentDictionary<string, IStringLocalizer>();
 
 
-        public BlogStringLocalizerFactory(IOptions<LocalizationConfigSection> localizationConfigSection)
-        {
-            _localizationConfigSection = localizationConfigSection ?? throw new ArgumentNullException(nameof(localizationConfigSection));
-        }
+		public BlogStringLocalizerFactory(IHostingEnvironment hostingEnvironment, IOptions<LocalizationConfigSection> localizationConfigSection)
+		{
+			_hostingEnvironment = hostingEnvironment ?? throw new ArgumentNullException(nameof(hostingEnvironment));
+			_localizationConfigSection = localizationConfigSection ?? throw new ArgumentNullException(nameof(localizationConfigSection));
+		}
 
-        public IStringLocalizer Create(Type resourceSource)
-        {
-            string resourceName = IgnoreOfficeLocalizerFactoryTypeNamespaceLocation(resourceSource.FullName);
+		public IStringLocalizer Create(Type resourceSource)
+		{
+			string resourceName = IgnoreOfficeLocalizerFactoryTypeNamespaceLocation(resourceSource.FullName);
 
-            if (_resourceLocalizations.Keys.Contains(resourceName))
-            {
-                return _resourceLocalizations[resourceName];
-            }
+			if (_resourceLocalizations.Keys.Contains(resourceName))
+			{
+				return _resourceLocalizations[resourceName];
+			}
 
-            return GetLocalizer(resourceName);
-        }
+			return GetLocalizer(resourceName);
+		}
 
-        private string IgnoreOfficeLocalizerFactoryTypeNamespaceLocation(string resourceSourceFullName)
-        {
-            if (resourceSourceFullName == null)
-            {
-                throw new ArgumentNullException(nameof(resourceSourceFullName));
-            }
+		private string IgnoreOfficeLocalizerFactoryTypeNamespaceLocation(string resourceSourceFullName)
+		{
+			if (resourceSourceFullName == null)
+			{
+				throw new ArgumentNullException(nameof(resourceSourceFullName));
+			}
 
-            if (_localizationConfigSection.Value.IgnoreOfficeLocalizerFactoryTypeNamespaceLocation
-                & !string.IsNullOrWhiteSpace(_localizationConfigSection.Value.OfficeLocalizerFactoryTypeNamespaceLocation))
-            {
-                return resourceSourceFullName.Replace(
-                    _localizationConfigSection.Value.OfficeLocalizerFactoryTypeNamespaceLocation + ".",
-                    "",
-                    StringComparison.InvariantCultureIgnoreCase);
-            }
+			if (_localizationConfigSection.Value.IgnoreOfficeLocalizerFactoryTypeNamespaceLocation
+				 & !string.IsNullOrWhiteSpace(_localizationConfigSection.Value.OfficeLocalizerFactoryTypeNamespaceLocation))
+			{
+				return resourceSourceFullName.Replace(
+					 _localizationConfigSection.Value.OfficeLocalizerFactoryTypeNamespaceLocation + ".",
+					 "",
+					 StringComparison.InvariantCultureIgnoreCase);
+			}
 
-            return resourceSourceFullName;
-        }
+			return resourceSourceFullName;
+		}
 
-        public IStringLocalizer Create(string baseName, string location)
-        {
-            string resourceName = IgnoreOfficeLocalizerFactoryTypeNamespaceLocation(baseName);
+		public IStringLocalizer Create(string baseName, string location)
+		{
+			string resourceName = IgnoreOfficeLocalizerFactoryTypeNamespaceLocation(baseName);
 
-            if (_resourceLocalizations.Keys.Contains(resourceName))
-            {
-                return _resourceLocalizations[resourceName];
-            }
+			if (_resourceLocalizations.Keys.Contains(resourceName))
+			{
+				return _resourceLocalizations[resourceName];
+			}
 
-            return GetLocalizer(resourceName);
-        }
+			return GetLocalizer(resourceName);
+		}
 
-        private IStringLocalizer GetLocalizer(string resourceName)
-        {
-            var officeStringLocalizer = new BlogStringLocalizer(GetAllFromOfficeFile(resourceName), resourceName);
-            return _resourceLocalizations.GetOrAdd(resourceName, officeStringLocalizer);
-        }
+		private IStringLocalizer GetLocalizer(string resourceName)
+		{
+			var officeStringLocalizer = new BlogStringLocalizer(GetAllFromOfficeFile(resourceName), resourceName);
+			return _resourceLocalizations.GetOrAdd(resourceName, officeStringLocalizer);
+		}
 
-        private Dictionary<string, string> GetAllFromOfficeFile(string resourceKey)
-        {
-            var dict = new Dictionary<string, string>();
-            dict.Add("Test1.pl-PL", "Test1.pl-PLv");
-            dict.Add("Test1.en-US", "Test1.pl-PLv");
-            dict.Add("Test1.de-DE", "Test1.pl-PLv");
-            return dict;
-        }
-    }
+		private Dictionary<string, string> GetAllFromOfficeFile(string resourceKey)
+		{
+			var resourcePath = Path.Combine(_hostingEnvironment.WebRootPath, _localizationConfigSection.Value.StringResourceFolder);
+			IOpenXMLStringResource stringRes = new OpenXMLStringResource(resourceKey, resourcePath);
+			var translations = stringRes.GetStrings();
+			return translations;
+		}
+	}
 }
--- Blog/Startup.cs
+++ Blog/Startup.cs
@@ -1,4 +1,20 @@
-﻿using DDDN.CrossBlog.Blog.Configuration;
+﻿/*
+* DDDN.CrossBlog.Blog.Startup
+* 
+* Copyright(C) 2017 Lukasz Jaskiewicz
+* Author: Lukasz Jaskiewicz (lukasz@jaskiewicz.de)
+*
+* This program is free software; you can redistribute it and/or modify it under the terms of the
+* GNU General Public License as published by the Free Software Foundation; version 2 of the License.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with this program; if not, write
+* to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+using DDDN.CrossBlog.Blog.Configuration;
 using DDDN.CrossBlog.Blog.Localization;
 using DDDN.CrossBlog.Blog.Routing;
 using Microsoft.AspNetCore.Builder;
@@ -13,85 +29,88 @@ using Microsoft.Extensions.Options;
 
 namespace DDDN.CrossBlog.Blog
 {
-    public class Startup
-    {
-        public IConfiguration Config { get; }
+	public class Startup
+	{
+		public IConfiguration Config { get; }
 
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                 .SetBasePath(env.ContentRootPath)
-                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
-            Config = builder.Build();
-        }
+		public Startup(IHostingEnvironment env)
+		{
+			var builder = new ConfigurationBuilder()
+				  .SetBasePath(env.ContentRootPath)
+				  .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+				  .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+			Config = builder.Build();
+		}
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddOptions();
-            services.Configure<LocalizationConfigSection>(Config.GetSection(ConfigSectionNames.Localization));
-            services.Configure<LocalizationConfigSection>(Config.GetSection(ConfigSectionNames.Routing));
-            services.AddScoped(cfg => cfg.GetService<IOptionsSnapshot<LocalizationConfigSection>>().Value);
-            services.AddScoped(cfg => cfg.GetService<IOptionsSnapshot<RoutingConfigSection>>().Value);
-            services.TryAddSingleton<IStringLocalizerFactory, BlogStringLocalizerFactory>();
-            services.TryAddSingleton<IStringLocalizer, BlogStringLocalizer>();
-            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            services.TryAddSingleton<IBlogCultures, BlogCultures>();
-            services.AddLocalization(options => options.ResourcesPath = "Resources");
-            services.AddRouting(options => options.LowercaseUrls = false);
-            services.AddMvc()
-                .AddViewLocalization();
-        }
+		public void ConfigureServices(IServiceCollection services)
+		{
+			var localizationConfigSection = Config.GetSection(ConfigSectionNames.Localization);
+			var stringResourceFolder = localizationConfigSection.Get<LocalizationConfigSection>().StringResourceFolder;
 
-        public void Configure(
-             IApplicationBuilder app,
-             IHostingEnvironment env,
-             IBlogCultures blogCultures,
-             IOptions<RoutingConfigSection> routingSection)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			services.AddOptions();
+			services.Configure<LocalizationConfigSection>(localizationConfigSection);
+			services.Configure<LocalizationConfigSection>(Config.GetSection(ConfigSectionNames.Routing));
+			services.AddScoped(cfg => cfg.GetService<IOptionsSnapshot<LocalizationConfigSection>>().Value);
+			services.AddScoped(cfg => cfg.GetService<IOptionsSnapshot<RoutingConfigSection>>().Value);
+			services.TryAddSingleton<IStringLocalizerFactory, BlogStringLocalizerFactory>();
+			services.TryAddSingleton<IStringLocalizer, BlogStringLocalizer>();
+			services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+			services.TryAddSingleton<IBlogCultures, BlogCultures>();
+			services.AddLocalization(options => options.ResourcesPath = stringResourceFolder);
+			services.AddRouting(options => options.LowercaseUrls = false);
+			services.AddMvc()
+				 .AddViewLocalization();
+		}
 
-            app.UseStaticFiles();
+		public void Configure(
+			  IApplicationBuilder app,
+			  IHostingEnvironment env,
+			  IBlogCultures blogCultures,
+			  IOptions<RoutingConfigSection> routingSection)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute
-                    (
-                         name: RouteNames.Default,
-                         template: routingSection.Value.DefaultRouteTemplate,
-                         defaults: new { },
-                         constraints: new
-                         {
-                             area = $@"^{routingSection.Value.BlogAreas}",
-                             culture = $@"^{blogCultures.SupportedCulturesDelimitedString}",
-                             id = @"\d+"
-                         },
-                         dataTokens: new
-                         {
-                             RouteName = RouteNames.Default
-                         }
-                    );
+			app.UseStaticFiles();
 
-                routes.MapRoute
-                    (
-                         name: RouteNames.Redirect,
-                         template: routingSection.Value.RedirectRouteTemplate,
-                         defaults: new
-                         {
-                             area = RouteNames.Redirect,
-                             controller = RouteNames.Redirect,
-                             action = RouteNames.Redirect
-                         },
-                         constraints: new { },
-                         dataTokens: new
-                         {
-                             RouteName = RouteNames.Default
-                         }
-                    );
-            });
-        }
-    }
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute
+						 (
+								name: RouteNames.Default,
+								template: routingSection.Value.DefaultRouteTemplate,
+								defaults: new { },
+								constraints: new
+								{
+									area = $@"^{routingSection.Value.BlogAreas}",
+									culture = $@"^{blogCultures.SupportedCulturesDelimitedString}",
+									id = @"\d+"
+								},
+								dataTokens: new
+								{
+									RouteName = RouteNames.Default
+								}
+						 );
+
+				routes.MapRoute
+						 (
+								name: RouteNames.Redirect,
+								template: routingSection.Value.RedirectRouteTemplate,
+								defaults: new
+								{
+									area = RouteNames.Redirect,
+									controller = RouteNames.Redirect,
+									action = RouteNames.Redirect
+								},
+								constraints: new { },
+								dataTokens: new
+								{
+									RouteName = RouteNames.Default
+								}
+						 );
+			});
+		}
+	}
 }

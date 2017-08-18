09a9f12e3a21d319c3001924423865b562983f07
Lukasz Jaskiewicz <lukasz@jaskiewicz.de>
2017-08-18 15:34:06 +0200

    moved copying img files from msbuild to gulp

--- src/DDDN.CrossBlog.Blog/gulpfile.js
+++ src/DDDN.CrossBlog.Blog/gulpfile.js
@@ -1,2 +1,9 @@
-﻿var gulp = require("gulp");
+﻿/// <binding BeforeBuild='copyFiles' />
 
+var gulp = require("gulp");
+
+gulp.task('copyFiles', function () {
+    gulp
+        .src('img/*.*')
+        .pipe(gulp.dest('wwwroot/img'));
+});

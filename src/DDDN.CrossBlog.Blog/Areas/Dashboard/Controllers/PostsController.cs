f362374344716e2f16c70670d3309b2e6e41ee0d
Lukasz Jaskiewicz <devdone@outlook.com>
2017-08-26 13:11:55 +0200

    added new dashboard area

---
+++ src/DDDN.CrossBlog.Blog/Areas/Dashboard/Controllers/PostsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DDDN.CrossBlog.Blog.Model;
+using DDDN.CrossBlog.Blog.Routing;
+
+namespace DDDN.CrossBlog.Blog.Areas.Dashboard.Controllers
+{
+	[Area("Dashboard")]
+	[MiddlewareFilter(typeof(BlogCulturesMiddlewareFilter))]
+	public class PostsController : Controller
+	{
+		private readonly CrossBlogContext _context;
+
+		public PostsController(CrossBlogContext context)
+		{
+			_context = context;
+		}
+
+		// GET: Dashboard/Posts
+		public async Task<IActionResult> Index()
+		{
+			return View(await _context.Posts.ToListAsync());
+		}
+
+		// GET: Dashboard/Posts/Details/5
+		public async Task<IActionResult> Details(Guid? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var post = await _context.Posts
+				 .SingleOrDefaultAsync(m => m.PostId == id);
+			if (post == null)
+			{
+				return NotFound();
+			}
+
+			return View(post);
+		}
+
+		// GET: Dashboard/Posts/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: Dashboard/Posts/Create
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("PostId,State,Created,Title")] Post post)
+		{
+			if (ModelState.IsValid)
+			{
+				post.PostId = Guid.NewGuid();
+				_context.Add(post);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(post);
+		}
+
+		// GET: Dashboard/Posts/Edit/5
+		public async Task<IActionResult> Edit(Guid? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var post = await _context.Posts.SingleOrDefaultAsync(m => m.PostId == id);
+			if (post == null)
+			{
+				return NotFound();
+			}
+			return View(post);
+		}
+
+		// POST: Dashboard/Posts/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(Guid id, [Bind("PostId,State,Created,Title")] Post post)
+		{
+			if (id != post.PostId)
+			{
+				return NotFound();
+			}
+
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(post);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!PostExists(post.PostId))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(post);
+		}
+
+		// GET: Dashboard/Posts/Delete/5
+		public async Task<IActionResult> Delete(Guid? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var post = await _context.Posts
+				 .SingleOrDefaultAsync(m => m.PostId == id);
+			if (post == null)
+			{
+				return NotFound();
+			}
+
+			return View(post);
+		}
+
+		// POST: Dashboard/Posts/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(Guid id)
+		{
+			var post = await _context.Posts.SingleOrDefaultAsync(m => m.PostId == id);
+			_context.Posts.Remove(post);
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+		private bool PostExists(Guid id)
+		{
+			return _context.Posts.Any(e => e.PostId == id);
+		}
+	}
+}
--- src/DDDN.CrossBlog.Blog/gulpfile.js
+++ src/DDDN.CrossBlog.Blog/gulpfile.js
@@ -1,159 +1,190 @@
 ﻿/// <binding BeforeBuild='_beforeBuild' />
 
 var gulp = require('gulp'),
-    notify = require('gulp-notify'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    cleanCSS = require('gulp-clean-css'),
-    sourcemaps = require('gulp-sourcemaps'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    plumber = require('gulp-plumber'),
-    rename = require('gulp-rename');
+	notify = require('gulp-notify'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	cleanCSS = require('gulp-clean-css'),
+	sourcemaps = require('gulp-sourcemaps'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	plumber = require('gulp-plumber'),
+	rename = require('gulp-rename');
 
 gulp.task('_beforeBuild', [
-    'copyFromNodeFontawesomeFontfolderToAssets', 'copyFontsToWwwroot',
-    'scssBuildBootstrap', 'scssBuildFontawesome',
-    'copyLocalizationDocumentsFromAreasToWwwroot', 'copyLocalizationDocumentsFromModelToWwwroot',
-    'copyAssetImageFiles',
-    'bundleCssBlog', 'bundleCssAdministration',
-    'copyNpmJsFilesToAssetsFolder', 'bundleBlogBodyJsFiles', 'bundleAdministrationBodyJsFiles',
-    'copyJqueryValidationLocalizationFolderFromNpmToAssets', 'copyJqueryValidationLocalizationFolderFromAssetsToWwwroot']);
+	'copyFromNodeFontawesomeFontfolderToAssets', 'copyFontsToWwwroot',
+	'scssBuildBootstrap', 'scssBuildFontawesome',
+	'copyLocalizationDocumentsFromAreasToWwwroot', 'copyLocalizationDocumentsFromModelToWwwroot',
+	'copyAssetImageFiles',
+	'bundleCssBlog', 'bundleCssDashboard', 'bundleCssAdministration',
+	'copyNpmJsFilesToAssetsFolder', 'bundleBlogBodyJsFiles', 'bundleDashboardBodyJsFiles', 'bundleAdministrationBodyJsFiles',
+	'copyJqueryValidationLocalizationFolderFromNpmToAssets', 'copyJqueryValidationLocalizationFolderFromAssetsToWwwroot']);
 
 /// compile Bootstrap scss
 gulp.task('scssBuildBootstrap', function () {
-    return gulp.src('node_modules/bootstrap/scss/bootstrap.scss')
-        .pipe(sass())
-        .pipe(autoprefixer())
-        .pipe(gulp.dest('assets/styles'));
+	return gulp.src('node_modules/bootstrap/scss/bootstrap.scss')
+		.pipe(sass())
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('assets/styles'));
 });
 
 /// compile Font Awesome scss
 gulp.task('scssBuildFontawesome', function () {
-    return gulp.src('node_modules/font-awesome/scss/font-awesome.scss')
-        .pipe(sass())
-        .pipe(autoprefixer())
-        .pipe(plumber())
-        .pipe(gulp.dest('assets/styles'));
+	return gulp.src('node_modules/font-awesome/scss/font-awesome.scss')
+		.pipe(sass())
+		.pipe(autoprefixer())
+		.pipe(plumber())
+		.pipe(gulp.dest('assets/styles'));
 });
 
 /// copy resource files from Areas to wwwroot
 gulp.task('copyLocalizationDocumentsFromAreasToWwwroot', function () {
-    gulp
-        .src('Areas/**/l10n/*.*')
-        .pipe(gulp.dest('wwwroot/l10n/Areas'));
+	gulp
+		.src('Areas/**/l10n/*.*')
+		.pipe(gulp.dest('wwwroot/l10n/Areas'));
 });
 
 /// copy resource files from Model to wwwroot
 gulp.task('copyLocalizationDocumentsFromModelToWwwroot', function () {
-    gulp
-        .src('Model/**/l10n/*.*')
-        .pipe(gulp.dest('wwwroot/l10n/Model'));
+	gulp
+		.src('Model/**/l10n/*.*')
+		.pipe(gulp.dest('wwwroot/l10n/Model'));
 });
 
 /// copy image assets to wwwroot
 gulp.task('copyAssetImageFiles', function () {
-    gulp
-        .src('assets/app/images/*.*')
-        .pipe(gulp.dest('wwwroot/images'));
+	gulp
+		.src('assets/app/images/*.*')
+		.pipe(gulp.dest('wwwroot/images'));
 });
 
 /// copy JavaScript files from node_modules to the JavaScript assets folder
 var npmJsFiles = [
-    'node_modules/jquery/dist/jquery.js',
-    'node_modules/jquery-validation/dist/jquery.validate.js',
-    'node_modules/popper.js/dist/umd/popper.js',
-    'node_modules/bootstrap/dist/js/bootstrap.js'
+	'node_modules/jquery/dist/jquery.js',
+	'node_modules/jquery-validation/dist/jquery.validate.js',
+	'node_modules/popper.js/dist/umd/popper.js',
+	'node_modules/bootstrap/dist/js/bootstrap.js'
 ];
 gulp.task('copyNpmJsFilesToAssetsFolder', function () {
-    gulp
-        .src(npmJsFiles)
-        .pipe(gulp.dest('assets/scripts'));
+	gulp
+		.src(npmJsFiles)
+		.pipe(gulp.dest('assets/scripts'));
 });
 
 /// copy and clean Blog area CSS assets to wwwroot
 var assetsCssBlog = [
-    'assets/styles/font-awesome.css',
-    'assets/styles/bootstrap.css',
-    'assets/app/styles/blog.css'
+	'assets/styles/font-awesome.css',
+	'assets/styles/bootstrap.css',
+	'assets/app/styles/blog.css'
 ];
 gulp.task('bundleCssBlog', function () {
-    return gulp.src(assetsCssBlog)
-        .pipe(sourcemaps.init())
-        .pipe(concat('blog.bundle.min.css'))
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(sourcemaps.write('/'))
-        .pipe(gulp.dest('wwwroot/css'));
+	return gulp.src(assetsCssBlog)
+		.pipe(sourcemaps.init())
+		.pipe(concat('blog.bundle.min.css'))
+		.pipe(cleanCSS({ compatibility: 'ie8' }))
+		.pipe(sourcemaps.write('/'))
+		.pipe(gulp.dest('wwwroot/css'));
+});
+
+/// copy and clean Blog area CSS assets to wwwroot
+var assetsCssDashboard = [
+	'assets/styles/font-awesome.css',
+	'assets/styles/bootstrap.css',
+	'assets/app/styles/dashboard.css'
+];
+gulp.task('bundleCssDashboard', function () {
+	return gulp.src(assetsCssDashboard)
+		.pipe(sourcemaps.init())
+		.pipe(concat('dashboard.bundle.min.css'))
+		.pipe(cleanCSS({ compatibility: 'ie8' }))
+		.pipe(sourcemaps.write('/'))
+		.pipe(gulp.dest('wwwroot/css'));
 });
 
 /// copy and clean Administration area CSS assets to wwwroot
 var assetsCssAdministration = [
-    'assets/styles/font-awesome.css',
-    'assets/styles/bootstrap.css',
-    'assets/app/styles/administration.css'
+	'assets/styles/font-awesome.css',
+	'assets/styles/bootstrap.css',
+	'assets/app/styles/administration.css'
 ];
 gulp.task('bundleCssAdministration', function () {
-    return gulp.src(assetsCssAdministration)
-        .pipe(sourcemaps.init())
-        .pipe(concat('administration.bundle.min.css'))
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(sourcemaps.write('/'))
-        .pipe(gulp.dest('wwwroot/css'));
+	return gulp.src(assetsCssAdministration)
+		.pipe(sourcemaps.init())
+		.pipe(concat('administration.bundle.min.css'))
+		.pipe(cleanCSS({ compatibility: 'ie8' }))
+		.pipe(sourcemaps.write('/'))
+		.pipe(gulp.dest('wwwroot/css'));
 });
 
 /// copy and uglify JavaScript assets to wwwroot
 var assetsJsBlogBody = [
-    'assets/scripts/jquery.js',
-    'assets/scripts/popper.js',
-    'assets/scripts/bootstrap.js',
-    'assets/app/scripts/blog.js'
+	'assets/scripts/jquery.js',
+	'assets/scripts/popper.js',
+	'assets/scripts/bootstrap.js',
+	'assets/app/scripts/blog.js'
 ];
 gulp.task('bundleBlogBodyJsFiles', function () {
-    return gulp.src(assetsJsBlogBody)
-        .pipe(sourcemaps.init())
-        .pipe(concat('blog.body.bundle.min.js'))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('/'))
-        .pipe(gulp.dest('wwwroot/js'));
+	return gulp.src(assetsJsBlogBody)
+		.pipe(sourcemaps.init())
+		.pipe(concat('blog.body.bundle.min.js'))
+		.pipe(uglify())
+		.pipe(sourcemaps.write('/'))
+		.pipe(gulp.dest('wwwroot/js'));
+});
+
+/// copy and uglify JavaScript assets to wwwroot
+var assetsJsBlogDashboard = [
+	'assets/scripts/jquery.js',
+	'assets/scripts/popper.js',
+	'assets/scripts/bootstrap.js',
+	'assets/app/scripts/dashboard.js'
+];
+gulp.task('bundleDashboardBodyJsFiles', function () {
+	return gulp.src(assetsJsBlogDashboard)
+		.pipe(sourcemaps.init())
+		.pipe(concat('dashboard.body.bundle.min.js'))
+		.pipe(uglify())
+		.pipe(sourcemaps.write('/'))
+		.pipe(gulp.dest('wwwroot/js'));
 });
 
 /// copy and uglify JavaScript assets to wwwroot
 var assetsJsAdministrationBody = [
-    'assets/scripts/jquery.js',
-    'assets/scripts/jquery.validate.js',
-    'assets/scripts/popper.js',
-    'assets/scripts/bootstrap.js',
-    'assets/app/scripts/administration.js'
+	'assets/scripts/jquery.js',
+	'assets/scripts/jquery.validate.js',
+	'assets/scripts/popper.js',
+	'assets/scripts/bootstrap.js',
+	'assets/app/scripts/administration.js'
 ];
 gulp.task('bundleAdministrationBodyJsFiles', function () {
-    return gulp.src(assetsJsAdministrationBody)
-        .pipe(sourcemaps.init())
-        .pipe(concat('administration.body.bundle.min.js'))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('/'))
-        .pipe(gulp.dest('wwwroot/js'));
+	return gulp.src(assetsJsAdministrationBody)
+		.pipe(sourcemaps.init())
+		.pipe(concat('administration.body.bundle.min.js'))
+		.pipe(uglify())
+		.pipe(sourcemaps.write('/'))
+		.pipe(gulp.dest('wwwroot/js'));
 });
 
 /// copy fonts from font-awesome node package to assets folder
 gulp.task('copyFromNodeFontawesomeFontfolderToAssets', function () {
-    return gulp.src('node_modules/font-awesome/fonts/*.*')
-        .pipe(gulp.dest('assets/fonts'));
+	return gulp.src('node_modules/font-awesome/fonts/*.*')
+		.pipe(gulp.dest('assets/fonts'));
 });
 
 /// copy fonts from assets to wwwroot
 gulp.task('copyFontsToWwwroot', function () {
-    return gulp.src('assets/fonts/*.*')
-        .pipe(gulp.dest('wwwroot/fonts'));
+	return gulp.src('assets/fonts/*.*')
+		.pipe(gulp.dest('wwwroot/fonts'));
 });
 
 /// copy jQuery validation localization folder
 gulp.task('copyJqueryValidationLocalizationFolderFromNpmToAssets', function () {
-    return gulp.src('node_modules/jquery-validation/dist/localization/**/*.*')
-        .pipe(gulp.dest('assets/scripts/localization'));
+	return gulp.src('node_modules/jquery-validation/dist/localization/**/*.*')
+		.pipe(gulp.dest('assets/scripts/localization'));
 });
 
 /// copy jQuery validation localization folder
 gulp.task('copyJqueryValidationLocalizationFolderFromAssetsToWwwroot', function () {
-    return gulp.src('assets/scripts/localization/**/*.*')
-        .pipe(gulp.dest('wwwroot/js/localization'));
+	return gulp.src('assets/scripts/localization/**/*.*')
+		.pipe(gulp.dest('wwwroot/js/localization'));
 });

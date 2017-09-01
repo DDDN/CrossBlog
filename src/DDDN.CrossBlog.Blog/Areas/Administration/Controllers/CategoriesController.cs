5f143e45847961a3f0cb76573fcbbf6cb28ef3dc
Lukasz Jaskiewicz <devdone@outlook.com>
2017-09-01 17:14:41 +0200

    creating Categories works now

---
+++ src/DDDN.CrossBlog.Blog/Areas/Administration/Controllers/CategoriesController.cs
@@ -0,0 +1,62 @@
+﻿/*
+* DDDN.CrossBlog.Blog.Areas.Administration.Controllers.CategoriesController
+* 
+* Copyright(C) 2017 Lukasz Jaskiewicz
+* Author: Lukasz Jaskiewicz (lukasz@jaskiewicz.de, devdone@outlook.com)
+*
+* This program is free software; you can redistribute it and/or modify it under the terms of the
+* GNU General Public License as published by the Free Software Foundation; version 2 of the License.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with this program; if not, write
+* to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+using DDDN.CrossBlog.Blog.Models;
+using DDDN.CrossBlog.Blog.Routing;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDDN.CrossBlog.Blog.Areas.Administration.Controllers
+{
+   [Area("Administration")]
+   [MiddlewareFilter(typeof(BlogCulturesMiddlewareFilter))]
+   public class CategoriesController : Controller
+   {
+      private readonly CrossBlogContext _cxt;
+
+      public CategoriesController(CrossBlogContext context)
+      {
+         _cxt = context ?? throw new ArgumentNullException(nameof(context));
+      }
+
+      public async Task<IActionResult> Index()
+      {
+         var categoryNames = await _cxt.Categories.AsNoTracking().ToListAsync();
+         return View(categoryNames);
+      }
+
+      public async Task<IActionResult> Create(string Name)
+      {
+         if (!string.IsNullOrWhiteSpace(Name))
+         {
+            var category = new Category
+            {
+               CategoryId = Guid.NewGuid(),
+               Name = Name
+            };
+
+            _cxt.Categories.Add(category);
+            await _cxt.SaveChangesAsync();
+         }
+
+         return RedirectToAction(nameof(Index));
+      }
+   }
+}

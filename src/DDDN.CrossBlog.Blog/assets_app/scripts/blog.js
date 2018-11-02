aafdd2ad49c22a89a3aec699feb4f62fa186595a
Lukasz Jaskiewicz <lukasz@jaskiewicz.de>
2018-11-02 19:28:44 +0100

    blog template changes

--- src/DDDN.CrossBlog.Blog/assets_app/scripts/blog.js
+++ src/DDDN.CrossBlog.Blog/assets_app/scripts/blog.js
@@ -1,5 +1,22 @@
 ﻿"use strict";
 
 $(document).ready(function () {
-   HighlightActiveLink();
+	HighlightActiveLink();
+	resizeBg();
 });
+
+$(window).on("load", function () {
+	resizeBg();
+});
+
+function resizeBg() {
+	var footer = $("footer");
+	if (footer.length === 1) {
+		var positionFooter = footer.position();
+		var bgdiv = $('.bg-article');
+		if (bgdiv.length === 1) {
+			bgdiv.css('height', positionFooter.top - 115);
+			bgdiv.css('display', 'block');
+		}
+	}
+}
